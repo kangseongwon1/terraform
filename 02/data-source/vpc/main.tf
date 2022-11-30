798ee1b7ff3217bcc2fc0f6fab2a1bd7ac8da5e3
ksw7734 <ksw7734@gmail.com>
2022-11-30 18:34:42 +0900

    test

---
+++ 02/data-source/vpc/main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.14.0"
+
+  cidr = var.vpc_cidr_block
+
+  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  #azs             = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e"]
+  private_subnets = slice(var.private_subnet_cidr_blocks, 0, 2)
+  public_subnets  = slice(var.public_subnet_cidr_blocks, 0, 2)
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
+
+  map_public_ip_on_launch = false
+}
+
+module "app_security_group" {
+  source  = "terraform-aws-modules/security-group/aws//modules/web"
+  version = "4.9.0"
+
+  name        = "web-server-sg"
+  description = "Security group for web-servers with HTTP ports open within VPC"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = module.vpc.public_subnets_cidr_blocks
+}
+
+module "lb_security_group" {
+  source  = "terraform-aws-modules/security-group/aws//modules/web"
+  version = "4.9.0"
+
+  name        = "lb-sg-project-alpha-dev"
+  description = "Security group for load balancer with HTTP ports open within VPC"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+}
+
+data "aws_region" "current"{}
